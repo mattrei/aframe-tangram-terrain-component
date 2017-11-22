7335e0088cdae5d533ab0bfb2593a2b990236210
mattre <mat@quorum.at>
2017-11-22 21:24:30 +0100

    fixing webpack issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   entry: './index.js',
   module: {
+    // Apply `noParse` to Tangram to prevent mangling of UMD boilerplate
+    noParse: /tangram\/dist\/tangram/,
     loaders: [{
       test: /\.yaml/,
             // exclude: /(node_modules)/,
-      loader: 'yml'
+      loader: 'yml-loader'
     }]
   }
 };
