75c9454ce992bab423b9fa1439542e604bd8a487
mattre <mat@quorum.at>
2019-04-28 19:13:42 +0200

    update versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   entry: './index.js',
   module: {
-    // Apply `noParse` to Tangram to prevent mangling of UMD boilerplate
-    noParse: /tangram\/dist\/tangram/,
     loaders: [{
       test: /\.yaml/,
             // exclude: /(node_modules)/,
